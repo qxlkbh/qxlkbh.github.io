db8a86ee3e8fb2c899e3703ea44140606558692f
SuffixAutomata <piggydan628318@protonmail.com>
2022-08-01 21:16:19 +0800

    minor amounts of tomfoolery.

--- build.py
+++ build.py
@@ -1,50 +1,65 @@
 import os
 import shutil
 import distutils.dir_util
+import re
 
-glo = {}
+
+class Holder():
+  pass
+
+
+glo = Holder()
 
 # setupdynamic
 _lc = 1
 while str(_lc + 1) in os.listdir("source/comic"):
   _lc += 1
-glo["lastComic"] = str(_lc)
+glo.lastComic = str(_lc)
 
 firstComic = 1
-lastComic = int(glo["lastComic"])
+lastComic = int(glo.lastComic)
 
 
 def path_to_comic(comic: int):
   return "/" + str(comic) + ".html"
 
 
-def substitute(template: str, setup: dict, fullpath: str):
-  dynamic = {"navbar": ""}
+def resolve(var: str, context: dict, fullpath: str):
+  ___ = Holder()
+  for i in context:
+    var = var.replace(i, "___." + i)
+    vars(___)[i] = context[i]
+  try:
+    return eval(var)
+  except Exception as e:
+    print("Warning: error %s on %s" % (str(e), fullpath))
+    return ""
+
+
+def substitute(template: str, setup: Holder, fullpath: str):
+  dynamic = Holder()
   if template == "comic":
-    pg = setup["pg"]
+    dynamic.navbar = ""
+    pg = setup.pg
     try:
       pg = int(pg)
       if pg == lastComic:
-        dynamic["navbar"] = '<a href="%s">&lt;</a> <a href="%s">&lt;&lt;</a>' % \
+        dynamic.navbar = '<a href="%s">&lt;</a> <a href="%s">&lt;&lt;</a>' % \
           (path_to_comic(pg - 1), path_to_comic(firstComic))
       elif pg == firstComic:
-        dynamic["navbar"] = '<a href="%s">&gt;&gt;</a> <a href="%s">&gt;</a>' % \
+        dynamic.navbar = '<a href="%s">&gt;&gt;</a> <a href="%s">&gt;</a>' % \
           (path_to_comic(lastComic), path_to_comic(pg + 1))
       else:
-        dynamic["navbar"] = '<a href="?pg=%s">&gt;&gt;</a> <a href="%s">&gt;</a> <a href="%s">&lt;</a> <a href="%s">&lt;&lt;</a>' %\
+        dynamic.navbar = '<a href="?pg=%s">&gt;&gt;</a> <a href="%s">&gt;</a> <a href="%s">&lt;</a> <a href="%s">&lt;&lt;</a>' %\
           (path_to_comic(lastComic), path_to_comic(pg + 1),
            path_to_comic(pg - 1), path_to_comic(firstComic))
     except ValueError:
       pass
+  context = {"glo": glo, "dynamic": dynamic, "setup": setup}
   src = open(f"templates/{template}").read()
-  for x in setup:
-    src = src.replace("{{ setup.%s }}" % x, setup[x])
-  for x in glo:
-    src = src.replace("{{ glo.%s }}" % x, glo[x])
-  for x in dynamic:
-    src = src.replace("{{ dynamic.%s }}" % x, dynamic[x])
-  if "{{" in src:
-    print("Warning: %s has unreplaced variables", fullpath)
+  s = re.finditer("\{\{ (.*?) \}\}", src)
+  for i in s:
+    src = src.replace(i.group(0), resolve(i.group(1), context, fullpath))
   return src
 
 
@@ -59,14 +74,14 @@ shutil.copy("qxlkbh.png", "build")
 # substitue
 for temp in os.listdir("source"):
   for sub in os.listdir(f"source/{temp}"):
-    setup = {"": ""}
+    setup = Holder()
     curvar = ""
     for i in open(f"source/{temp}/{sub}"):
       if i.strip().endswith("==="):
         curvar = i.strip()[:-3]
-        setup[curvar] = ""
+        vars(setup)[curvar] = ""
       else:
-        setup[curvar] += i
+        vars(setup)[curvar] += i
     comp = substitute(temp, setup, f"source/{temp}/{sub}")
     open(f"build/{sub}.html", "w").write(comp)
 
